80043bd6266b4c255ec4a3f0602e10efe31fec68
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-05-10 16:36:20 -0500

    Listo el calculo de Na y Nb

--- src/proyecto/TratamientoDatos.java
+++ src/proyecto/TratamientoDatos.java
@@ -2,6 +2,7 @@ package proyecto;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -11,6 +12,7 @@ public class TratamientoDatos {
 	public TratamientoDatos(HashMap<String, String> map) {
 		valores = new ArrayList<HashMap<Character, Integer>>();
 		crearMapa(map);
+		test(map);
 	}
 	
 	private void crearMapa(HashMap<String, String> map) {
@@ -41,6 +43,73 @@ public class TratamientoDatos {
 		}
 	}
 	
+	private void test(HashMap<String, String> map) {
+		Set<String> keys = map.keySet();
+		TreeSet<String> sortedKeys = new TreeSet<String>(keys);
+		ArrayList<String> values = new ArrayList<String>();
+		String[] formulas = new String[keys.size()];
+		char[] elementos;
+		int index = 0;
+		for(String lines : sortedKeys) {
+			values.add(map.get(lines));
+			System.out.println(values.get(index));
+			index++;
+		}
+		index = 1;
+		for(int i = 0; i < values.size(); i++)
+			for(int j = i + 1; j < values.size(); j++) {
+				T(values.get(i), values.get(j));
+				System.out.println(index++ + "\t1 => " + values.get(i) + "\t2 => " + values.get(j));
+			}
+	}
+	
+	private void T(String a, String b) {
+		char[] compuestoA = a.toCharArray();
+		char[] compuestoB = b.toCharArray();
+		HashMap<Character, Integer> mapaA = sacarCaracteres(compuestoA);
+		HashMap<Character, Integer> mapaB = sacarCaracteres(compuestoB);
+		int Na = numeroElementos(mapaA);
+		int Nb = numeroElementos(mapaB);
+		int Nc = numeroElementosComunes(mapaA, mapaB);
+		System.out.println("Na = " + Na + "\tNb = " + Nb);
+		mostrarMapa(mapaA);
+	}
+
+	private HashMap<Character, Integer> sacarCaracteres(char[] compuesto) {
+		HashMap<Character, Integer> charMap = new HashMap<Character, Integer>();
+		for(char l : compuesto) {
+			if(charMap.containsKey(l)) {
+				int contador = charMap.get(l);
+				charMap.put(l, contador + 1);
+			} else
+				charMap.put(l, 1);
+			if(charMap.containsKey('@'))
+				charMap.put('@', 1);
+		}
+		return charMap;
+	}
+	
+	private int numeroElementos(HashMap<Character, Integer> map) {
+		Set<Character> keys = map.keySet();
+		TreeSet<Character> sortedKeys = new TreeSet<Character>(keys);
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		int N = 0;
+		for(char lines : sortedKeys)
+			values.add(map.get(lines));
+		for(int lines : values)
+			N += lines;
+		return N;
+	}
+	
+	private int numeroElementosComunes(HashMap<Character, Integer> mapA, HashMap<Character, Integer> mapB) {
+		Set<Character> keysA = mapA.keySet();
+		TreeSet<Character> sortedKeysA = new TreeSet<Character>(keysA);
+		Set<Character> keysB = mapB.keySet();
+		TreeSet<Character> sortedKeysB = new TreeSet<Character>(keysB);
+		
+		return 0;
+	}
+
 	private void mostrarMapa(HashMap<Character, Integer> map) {
 		Set<Character> keys = map.keySet();
 		TreeSet<Character> sortedKeys = new TreeSet<Character>(keys);
