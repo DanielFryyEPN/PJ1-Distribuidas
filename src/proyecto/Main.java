2e72f02929b30b8924ff9318971e68393ced4605
DanielFryyEPN <daniel.freire01@epn.edu.ec>
2017-05-11 18:38:41 -0500

    nuevos archivos fuente

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Formatear p = new Formatear();
-		p.leerArchivo("\\src\\archivos\\chemicals.tsv");
+		p.leerArchivo("\\src\\archivos\\ZINC_chemicals.tsv");
 	}
 }
